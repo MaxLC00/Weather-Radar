6f159bfa9456915cce57835d1ee692ed5e4e8517
MaxLC00 <167907843+MaxLC00@users.noreply.github.com>
2024-07-08 15:47:03 -0500

    added local storage and forecast

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,20 +1,46 @@
 const fetchButton = document.getElementById('search-button');
+let searchTermEl = document.getElementById('search-value');
 
 
 function getForecast () {
    // preventDefault()
-    console.log('button pressed')
+   let searchTerm = searchTermEl.value
    
-    //const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=aedff6b19ebb0a704cc38341604b8fc8&units=imperial"
-    const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=aedff6b19ebb0a704cc38341604b8fc8&units=imperial"
+    const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=aedff6b19ebb0a704cc38341604b8fc8&units=imperial"
     fetch(weatherUrl)
     .then(function (response) {
         return response.json();
-    })
-    .then(function (data) {
+    }).then(function (data) {
       console.log(data);
-    }
-)
+    })
+
+    const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=aedff6b19ebb0a704cc38341604b8fc8&units=imperial"
+    fetch(forecastUrl)
+    .then(function (response) {
+      return response.json();
+  }).then(function (data) {
+    console.log(data);
+  })
+
+    updateHistory();
+    displayWeather;
+    //displayForecast;
+  }
+
+  function updateHistory() {
+    const history = JSON.parse(localStorage.getItem("history")) || [];
+    let searchTerm = searchTermEl.value
+    console.log(history);
+     history.push(searchTerm);
+     localStorage.setItem('history', JSON.stringify(history));
+  }
+
+function displayWeather () {
+
+}
+
+function displayForecast () {
+
 }
 
 fetchButton.addEventListener('click', getForecast);
