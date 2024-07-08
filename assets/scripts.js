cb2aa98035b56c22051a8d1c17570950553ad9d7
MaxLC00 <167907843+MaxLC00@users.noreply.github.com>
2024-07-08 17:21:33 -0500

    added history display and current weather display

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,45 +1,78 @@
 const fetchButton = document.getElementById('search-button');
-let searchTermEl = document.getElementById('search-value');
+const searchTermEl = document.getElementById('search-value');
+const historyEl = document.getElementById('search-history');
+const displayEl = document.getElementById('today');
+const forecastEl = document.getElementById('five-day');
 
 
-function getForecast () {
-   // preventDefault()
-   let searchTerm = searchTermEl.value
-   
-    const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=aedff6b19ebb0a704cc38341604b8fc8&units=imperial"
-    fetch(weatherUrl)
+function getForecast() {
+  
+  updateHistory();
+  displayWeather();
+  // displayForecast();
+}
+
+
+function updateHistory() {
+  const history = JSON.parse(localStorage.getItem("history")) || [];
+  let searchTerm = searchTermEl.value
+
+ historyEl.innerHTML = "";
+
+ for (let i = 0; i < history.length; i++) {
+  console.log(history[i])
+   const termEl = document.createElement('li');
+    termEl.textContent = history[i];
+
+   historyEl.appendChild(termEl);
+    
+  }
+
+  history.push(searchTerm);
+  localStorage.setItem('history', JSON.stringify(history));
+}
+
+function displayWeather() {
+  let searchTerm = searchTermEl.value
+
+  displayEl.innerHTML = "";
+
+  const weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=aedff6b19ebb0a704cc38341604b8fc8&units=imperial"
+  fetch(weatherUrl)
     .then(function (response) {
-        return response.json();
+      return response.json();
+    }).then(function (data) {
+      console.log(data);
+    
+    const weatherEl = document.createElement('div');
+    weatherEl.classList = "card-body bg-info";
+    const tempToday = document.createElement('h1');
+    tempToday.textContent = "temperature: " + data.main.temp + "F"; 
+    const humToday = document.createElement('h2');
+    humToday.textContent = "humidity: " + data.main.humidity + "%";
+    const windToday = document.createElement('h2');
+    windToday.textContent = "Wind: " + data.main.humidity + "MPH";
+    const conToday = document.createElement('img');
+    conToday.src = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+
+   weatherEl.appendChild(tempToday);
+   weatherEl.appendChild(humToday);
+   weatherEl.appendChild(windToday);
+   weatherEl.appendChild(conToday);
+   displayEl.appendChild(weatherEl);
+    });
+}
+
+function displayForecast() {
+  let searchTerm = searchTermEl.value
+
+  const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=aedff6b19ebb0a704cc38341604b8fc8&units=imperial"
+  fetch(forecastUrl)
+    .then(function (response) {
+      return response.json();
     }).then(function (data) {
       console.log(data);
     })
-
-    const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&appid=aedff6b19ebb0a704cc38341604b8fc8&units=imperial"
-    fetch(forecastUrl)
-    .then(function (response) {
-      return response.json();
-  }).then(function (data) {
-    console.log(data);
-  })
-
-    updateHistory();
-    displayWeather;
-    //displayForecast;
-  }
-
-  function updateHistory() {
-    const history = JSON.parse(localStorage.getItem("history")) || [];
-    let searchTerm = searchTermEl.value
-    console.log(history);
-     history.push(searchTerm);
-     localStorage.setItem('history', JSON.stringify(history));
-  }
-
-function displayWeather () {
-
-}
-
-function displayForecast () {
 
 }
 
